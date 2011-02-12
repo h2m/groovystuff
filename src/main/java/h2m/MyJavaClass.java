525923f2e3194ff1ba425688958cf5f808bb82ee
mhaebich <mhaebich@nue-230-xp.polaris.senacor.com>
2011-02-12 18:00:12 +0100

    added call to scala class

--- src/main/java/h2m/MyJavaClass.java
+++ src/main/java/h2m/MyJavaClass.java
@@ -1,23 +1,23 @@
 package h2m;
 
 
-import org.apache.commons.logging.*;
-import org.apache.commons.logging.impl.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 
-public class MyJavaClass{
+public class MyJavaClass {
 
 
-	private static Log LOG = LogFactory.getLog(MyJavaClass.class);
+    private static Log LOG = LogFactory.getLog(MyJavaClass.class);
 
-	static{
-		LOG.debug("class initialised");
-	}
-	
-	public static void doIt(int i){
-		// MyScalaClass msc = new MyScalaClass(i);
-		String msc = "or without Scala " + i;
-		LOG.info("did it with: " + msc);
-	}
+    static {
+        LOG.debug("class initialised");
+    }
+
+    public static void doIt(int i) {
+        MyScalaClass msc = new MyScalaClass(i);
+        // String msc = "or without Scala " + i;
+        LOG.info("did it with: " + msc);
+    }
 
 }
